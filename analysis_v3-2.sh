100d2ecd4860c81e95f3c351a9ade31f82eb9731
chelseaju <chelseaju@ucla.edu>
2014-03-03 19:03:22 -0800

    shell scripts version 4

--- analysis_v3-2.sh
+++ analysis_v3-2.sh
@@ -25,77 +25,77 @@ LOOKUP=$DIR/"lookup_table.txt"
 
 echo ""
 echo "Step 1: Prepare for Lasso Analysis"
-#python unknown_unifier.py -o $DIR -t genes -d $DIR/$X10A10/$TOPOUT/ $DIR/$X10A30/$TOPOUT/ $DIR/$X10A50/$TOPOUT/ $DIR/$X20A10/$TOPOUT/ $DIR/$X20A30/$TOPOUT/ $DIR/$X20A50/$TOPOUT/ $DIR/$X30A10/$TOPOUT/ $DIR/$X30A30/$TOPOUT/ $DIR/$X30A50/$TOPOUT/
+python unknown_unifier.py -o $DIR -t genes -d $DIR/$X10A10/$TOPOUT/ $DIR/$X10A30/$TOPOUT/ $DIR/$X10A50/$TOPOUT/ $DIR/$X20A10/$TOPOUT/ $DIR/$X20A30/$TOPOUT/ $DIR/$X20A50/$TOPOUT/ $DIR/$X30A10/$TOPOUT/ $DIR/$X30A30/$TOPOUT/ $DIR/$X30A50/$TOPOUT/
 
-#python unknown_updator.py -d $DIR -f $DIR/$X10A10/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X10A10/$TOPOUT/genes_distribution.eqn -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X10A30/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X10A30/$TOPOUT/genes_distribution.eqn -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X10A50/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X10A50/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X10A10/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X10A10/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X10A30/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X10A30/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X10A50/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X10A50/$TOPOUT/genes_distribution.eqn -t genes
 
-#python unknown_updator.py -d $DIR -f $DIR/$X20A10/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X20A10/$TOPOUT/genes_distribution.eqn -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X20A30/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X20A30/$TOPOUT/genes_distribution.eqn -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X20A50/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X20A50/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X20A10/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X20A10/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X20A30/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X20A30/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X20A50/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X20A50/$TOPOUT/genes_distribution.eqn -t genes
 
-#python unknown_updator.py -d $DIR -f $DIR/$X30A10/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X30A10/$TOPOUT/genes_distribution.eqn -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X30A30/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X30A30/$TOPOUT/genes_distribution.eqn -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X30A50/$TOPOUT/genes_distribution.matrix -t genes
-#python unknown_updator.py -d $DIR -f $DIR/$X30A50/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X30A10/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X30A10/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X30A30/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X30A30/$TOPOUT/genes_distribution.eqn -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X30A50/$TOPOUT/genes_distribution.matrix -t genes
+python unknown_updator.py -d $DIR -f $DIR/$X30A50/$TOPOUT/genes_distribution.eqn -t genes
 
 
-#python unknown_unifier.py -o $DIR -t transcripts -d $DIR/$X10A10/$TOPOUT/ $DIR/$X10A30/$TOPOUT/ $DIR/$X10A50/$TOPOUT/ $DIR/$X20A10/$TOPOUT/ $DIR/$X20A30/$TOPOUT/ $DIR/$X20A50/$TOPOUT/ $DIR/$X30A10/$TOPOUT/ $DIR/$X30A30/$TOPOUT/ $DIR/$X30A50/$TOPOUT/
+python unknown_unifier.py -o $DIR -t transcripts -d $DIR/$X10A10/$TOPOUT/ $DIR/$X10A30/$TOPOUT/ $DIR/$X10A50/$TOPOUT/ $DIR/$X20A10/$TOPOUT/ $DIR/$X20A30/$TOPOUT/ $DIR/$X20A50/$TOPOUT/ $DIR/$X30A10/$TOPOUT/ $DIR/$X30A30/$TOPOUT/ $DIR/$X30A50/$TOPOUT/
 
-#python unknown_updator.py -d $DIR -f $DIR/$X10A10/$TOPOUT/transcripts_distribution.matrix -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X10A10/$TOPOUT/transcripts_distribution.eqn -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X10A30/$TOPOUT/transcripts_distribution.matrix -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X10A30/$TOPOUT/transcripts_distribution.eqn -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X10A50/$TOPOUT/transcripts_distribution.matrix -t transcripts 
-#python unknown_updator.py -d $DIR -f $DIR/$X10A50/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X10A10/$TOPOUT/transcripts_distribution.matrix -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X10A10/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X10A30/$TOPOUT/transcripts_distribution.matrix -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X10A30/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X10A50/$TOPOUT/transcripts_distribution.matrix -t transcripts 
+python unknown_updator.py -d $DIR -f $DIR/$X10A50/$TOPOUT/transcripts_distribution.eqn -t transcripts
 
-#python unknown_updator.py -d $DIR -f $DIR/$X20A10/$TOPOUT/transcripts_distribution.matrix -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X20A10/$TOPOUT/transcripts_distribution.eqn -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X20A30/$TOPOUT/transcripts_distribution.matrix -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X20A30/$TOPOUT/transcripts_distribution.eqn -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X20A50/$TOPOUT/transcripts_distribution.matrix -t transcripts 
-#python unknown_updator.py -d $DIR -f $DIR/$X20A50/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X20A10/$TOPOUT/transcripts_distribution.matrix -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X20A10/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X20A30/$TOPOUT/transcripts_distribution.matrix -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X20A30/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X20A50/$TOPOUT/transcripts_distribution.matrix -t transcripts 
+python unknown_updator.py -d $DIR -f $DIR/$X20A50/$TOPOUT/transcripts_distribution.eqn -t transcripts
 
-#python unknown_updator.py -d $DIR -f $DIR/$X30A10/$TOPOUT/transcripts_distribution.matrix -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X30A10/$TOPOUT/transcripts_distribution.eqn -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X30A30/$TOPOUT/transcripts_distribution.matrix -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X30A30/$TOPOUT/transcripts_distribution.eqn -t transcripts
-#python unknown_updator.py -d $DIR -f $DIR/$X30A50/$TOPOUT/transcripts_distribution.matrix -t transcripts 
-#python unknown_updator.py -d $DIR -f $DIR/$X30A50/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X30A10/$TOPOUT/transcripts_distribution.matrix -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X30A10/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X30A30/$TOPOUT/transcripts_distribution.matrix -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X30A30/$TOPOUT/transcripts_distribution.eqn -t transcripts
+python unknown_updator.py -d $DIR -f $DIR/$X30A50/$TOPOUT/transcripts_distribution.matrix -t transcripts 
+python unknown_updator.py -d $DIR -f $DIR/$X30A50/$TOPOUT/transcripts_distribution.eqn -t transcripts
 
 
 echo ""
 echo "Step 2: Training Coefficient with Lasso"
-R --no-save --slave < lasso_trainer.R --args $DIR genes
+#R --no-save --slave < lasso_trainer.R --args $DIR genes
 
 
 echo ""
 echo "Step 3: Lasso Prediction"
-R --no-save --slave < lasso_predictor.R --args $DIR $X30A10/$TOPOUT genes glmnet
-R --no-save --slave < lasso_predictor.R --args $DIR $X30A30/$TOPOUT genes glmnet
-R --no-save --slave < lasso_predictor.R --args $DIR $X30A50/$TOPOUT genes glmnet
-R --no-save --slave < lasso_predictor.R --args $DIR $X30A10/$TOPOUT genes genlasso
-R --no-save --slave < lasso_predictor.R --args $DIR $X30A30/$TOPOUT genes genlasso
-R --no-save --slave < lasso_predictor.R --args $DIR $X30A50/$TOPOUT genes genlasso
+#R --no-save --slave < lasso_predictor.R --args $DIR $X30A10/$TOPOUT genes glmnet
+#R --no-save --slave < lasso_predictor.R --args $DIR $X30A30/$TOPOUT genes glmnet
+#R --no-save --slave < lasso_predictor.R --args $DIR $X30A50/$TOPOUT genes glmnet
+#R --no-save --slave < lasso_predictor.R --args $DIR $X30A10/$TOPOUT genes genlasso
+#R --no-save --slave < lasso_predictor.R --args $DIR $X30A30/$TOPOUT genes genlasso
+#R --no-save --slave < lasso_predictor.R --args $DIR $X30A50/$TOPOUT genes genlasso
 
 
 echo ""
 echo "Step 4: Lasso Validation"
-R --no-save --slave < lasso_validator.R --args $DIR $X30A10/$TOPOUT genes glmnet
-R --no-save --slave < lasso_validator.R --args $DIR $X30A30/$TOPOUT genes glmnet
-R --no-save --slave < lasso_validator.R --args $DIR $X30A50/$TOPOUT genes glmnet
-R --no-save --slave < lasso_validator.R --args $DIR $X30A10/$TOPOUT genes genlasso
-R --no-save --slave < lasso_validator.R --args $DIR $X30A30/$TOPOUT genes genlasso
-R --no-save --slave < lasso_validator.R --args $DIR $X30A50/$TOPOUT genes genlasso
+#R --no-save --slave < lasso_validator.R --args $DIR $X30A10/$TOPOUT genes glmnet
+#R --no-save --slave < lasso_validator.R --args $DIR $X30A30/$TOPOUT genes glmnet
+#R --no-save --slave < lasso_validator.R --args $DIR $X30A50/$TOPOUT genes glmnet
+#R --no-save --slave < lasso_validator.R --args $DIR $X30A10/$TOPOUT genes genlasso
+#R --no-save --slave < lasso_validator.R --args $DIR $X30A30/$TOPOUT genes genlasso
+#R --no-save --slave < lasso_validator.R --args $DIR $X30A50/$TOPOUT genes genlasso
 
 
 
--- analysis_v3.sh
+++ analysis_v3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-## bash analysis_v3.sh DIR COVERAGE READLEN ABUNDANCE
-## ex bash analysis_v3.sh select_one_pseudogene_110 1 100 10
+## bash analysis_v4.sh DIR COVERAGE READLEN ABUNDANCE
+## ex bash analysis_v4.sh select_one_pseudogene_110 1 100 10
 
 DIR=$1
 COVERAGE=$2
@@ -16,7 +16,7 @@ CHROMOSOME=('chr1' 'chr2' 'chr3' 'chr4' 'chr5' 'chr6' 'chr7' 'chr8'
 
 #CHROMOSOME=('chr1' 'chr2')
 
-echo "Data Analysis Version 3:"
+echo "Data Analysis Version 4:"
 
 echo "Step 0: Folders and Files Preparation"
 CMD0="mkdir -p $DIR/$SUBDIR/$TOPOUT/mapping"
@@ -33,53 +33,28 @@ $CMD1
 ## Compute the distribution of read generated from parent genes
 
 echo ""
-echo "Step 1: Read Counts For Transcripts"
-
-# expect read count
-python expected_counter.py -d $DIR/$SUBDIR/$TOPOUT 
-
-for chr in "${CHROMOSOME[@]}"
-do
-  echo ""
-  echo "CHROMOSOME ${chr} =========================================="
-  python exon_identifier.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr}
-  python transcript_identifier.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr}
-  python observed_counter.py  -d $DIR/$SUBDIR/$TOPOUT -c ${chr} -t transcripts
-  python observed_separator.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr} -t transcripts
-
-done
-
-# observed read distribution
-python distribution_equation.py -d $DIR/$SUBDIR/$TOPOUT -t transcripts
-python distribution_matrix.py -d $DIR/$SUBDIR/$TOPOUT -t transcripts
-#python distribution_coefficient_merger.py -d $DIR/$SUBDIR/$TOPOUT/transcripts_distribution.matrix -c $DIR/transcripts_lasso_coefficient.xls
-
-echo ""
-echo "Step 2: Read Counts for Genes"
+echo "Step 1: Read Counts for Genes"
 
 # expect read count
 python expected_counter.py -d $DIR/$SUBDIR/$TOPOUT
-python ENSP2ENSG.py -d $DIR/$SUBDIR/$TOPOUT -i transcripts_expected_read_count.txt -o genes_expected_read_count.txt
+python /home/chelseaju/Database/EnsemblTranscriptome/Genes/ENST2ENSG.py -i expected_read_count.txt -o ENSG_expected_read_count.txt -d $DIR/$SUBDIR/$TOPOUT
 
 for chr in "${CHROMOSOME[@]}"
 do
   echo ""
   echo "CHROMOSOME ${chr} =========================================="
   
-  python exon_identifier.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr}
-  python gene_identifier.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr}
-  python observed_counter.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr} -t genes
+  python exon_identifier_v2.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr}
+  python gene_identifier_v2.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr}
   python observed_separator.py -d $DIR/$SUBDIR/$TOPOUT -c ${chr} -t genes
-  python ENSP2ENSG.py -d $DIR/$SUBDIR/$TOPOUT/mapping -i ${chr}_genes_distribution.txt -o ${chr}_genes_distribution.txt.new
-  mv $DIR/$SUBDIR/$TOPOUT/mapping/${chr}_genes_distribution.txt.new $DIR/$SUBDIR/$TOPOUT/mapping/${chr}_genes_distribution.txt
+  python /home/chelseaju/Database/EnsemblTranscriptome/Genes/ENSG2PGOHUM.py -d $DIR/$SUBDIR/$TOPOUT/mapping -i ${chr}_genes_distribution.txt -o ${chr}_PGOHUM_distribution.txt
+  python /home/chelseaju/Database/EnsemblTranscriptome/Genes/ENST2ENSG.py -d $DIR/$SUBDIR/$TOPOUT/mapping -i ${chr}_PGOHUM_distribution.txt -o ${chr}_ENSG_distribution.txt
 
 done
 
 # observed read distribution
-python distribution_equation.py -d $DIR/$SUBDIR/$TOPOUT -t genes
-mv $DIR/$SUBDIR/$TOPOUT/genes_distribution.eqn $DIR/$SUBDIR/$TOPOUT/genes_distribution.eqn.backup
-python ENSP2ENSG.py -d $DIR/$SUBDIR/$TOPOUT -i genes_distribution.eqn.backup -o genes_distribution.eqn
-python distribution_matrix.py -d $DIR/$SUBDIR/$TOPOUT -t genes
+python distribution_equation.py -d $DIR/$SUBDIR/$TOPOUT -t ENSG_PGOHUM
+python distribution_matrix.py -d $DIR/$SUBDIR/$TOPOUT -t ENSG_PGOHUM
 
 
 
