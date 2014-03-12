61136dce7633b379047c5c84f8c5510ab7d06272
chelseaju <chelseaju@ucla.edu>
2014-03-12 01:12:59 -0700

    add new scripts for data preparation

--- preparation.sh
+++ preparation.sh
@@ -14,8 +14,10 @@ GENOMEINDEX="/u/home/c/chelseaj/project/database/Bowtie2Index/"
 CUFFLINKS="cufflinks_out"
 TOPOUT="tophat_out"
 SUBDIR=$COVERAGE"X_"$READLEN"L_"$ABUNDANCE"A"
-GTF="SELECT_GENES.gtf"
+GTF="$DIR/SELECT_GENES.gtf"
 SIMCONFIG="$DIR/$SUBDIR/config.txt"
+GENELIST="$DIR/SELECT_GENES.txt"
+EXPRESSION="$DIR/$SUBDIR/abundance.txt"
 
 ## Step 0: Create directory
 echo ""
@@ -24,10 +26,34 @@ echo "Step 0 - Creating Directories"
 mkdir -p $DIR/$SUBDIR/$TOPOUT
 mkdir -p $DIR/$SUBDIR/$CUFFLINKS
 
+echo ""
 echo "$DIR/$SUBDIR/$TOPOUT created"
 echo "$DIR/$SUBDIR/$CUFFLINKS created"
 
+## Step 1: Generate gene abundance file, and simulate reads
 echo ""
 echo "Step 1 - Generating Reads"
-CMD1="RNAseqSim-CJ.jar $SIMCONFIG -GTF_File=$DIR/$GTF -FASTA_File=$GENOME -Chromosome=chr[0-9XYM]* -Chromosome_Matching=Fuzzy -Abundance_File=$PREFIX""_parents_abundance_$ABUNDANCE.txt -Abundance_Overwritten=No -Expressed_Transcript_Percentage=1 -SV_Allowed=No -Read_ID_Prefix=ENSP- -Read_Length=$READLEN -Coverage_Factor=$COVERAGE -Quality_Generator=Prefect -Flip_And_Reverse=Yes -Unknown_Factor=0.0001 -Output_Fastq_1=$DIR/$SUBDIR/1.fq -Output_Fastq_2=$DIR/$SUBDIR/2.fq"
+python abundance_assigner.py -i $GENELIST -a $ABUNDANCE -o $EXPRESSION
+java -Xmx1G -Xms1000m -jar /u/home/c/chelseaj/project/software/RNAseqSim-CJ.jar $SIMCONFIG -GTF_File=$GTF -FASTA_File=$GENOME -Chromosome=chr[0-9XYM]* -Chromosome_Matching=Fuzzy -Abundance_File=$EXPRESSION -Abundance_Overwritten=No -Expressed_Transcript_Percentage=1 -SV_Allowed=No -Read_ID_Prefix=ENST- -Read_Length=$READLEN -Coverage_Factor=$COVERAGE -Quality_Generator=Prefect -Flip_And_Reverse=Yes -Unknown_Factor=0.0001 -Output_Fastq_1=$DIR/$SUBDIR/1.fq -Output_Fastq_2=$DIR/$SUBDIR/2.fq
+
+
+## Step 2: Mapping the reads using tophat
+echo ""
+echo "Step 2 - Mapping Reads"
+#tophat2 -o $DIR/$SUBDIR/$TOPOUT $GENOMEINDEX $DIR/$SUBDIR/1.fq $DIR/$SUBDIR/2.fq
+
+
+# step 3: run cufflinks to compute FPKM
+echo ""
+echo "Step 3 - Assembling Transcripts"
+#cufflinks -o $DIR/$SUBDIR/$CUFFLINKS $DIR/$SUBDIR/$TOPOUT/accepted_hits.bam
+
+
+
+
+
+
+
+
+
 
