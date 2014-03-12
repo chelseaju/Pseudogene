d4036ca33adf240423d5d65489baf8e7936abfd8
chelseaju <chelseaju@ucla.edu>
2014-03-12 13:40:17 -0700

    Change chrM to chrMT

--- analysis_v4.sh
+++ analysis_v4.sh
@@ -12,7 +12,7 @@ SUBDIR=$COVERAGE"X_"$READLEN"L_"$ABUNDANCE"A"
 BAM="accepted_hits.bam"
 CHROMOSOME=('chr1' 'chr2' 'chr3' 'chr4' 'chr5' 'chr6' 'chr7' 'chr8'
 	'chr9' 'chr10' 'chr11' 'chr12' 'chr13' 'chr14' 'chr15' 'chr16'
-	'chr17' 'chr18' 'chr19' 'chr20' 'chr21' 'chr22' 'chrX' 'chrY' 'chrM')
+	'chr17' 'chr18' 'chr19' 'chr20' 'chr21' 'chr22' 'chrX' 'chrY' 'chrMT')
 
 ## MAC
 #ENST_ENSG_ENSP="/home/chelseaju/Database/Ensembl/ENST_ENSG_ENSP_74.txt"
