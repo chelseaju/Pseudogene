56d4da0e20ba80521bdd420e956d42346e91612a
chelseaju <chelseaju@ucla.edu>
2014-03-21 16:24:40 -0700

    add evaluation.sh for evaluating cuffdiff result

---
+++ evaluation.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+## sh evaluation.sh DIR A B BAM SUFFIX
+
+DIR=$1
+SAMPLEA=$2
+SAMPLEB=$3
+BAM=$4
+SUFFIX=$5
+
+TOPHAT="tophat_out"
+CUFFLINK=cufflinks_$SUFFIX
+CUFFMERGE=$DIR/$SAMPLEA"_"$SAMPLEB/cuffdiff_$SUFFIX
+ASSEMBLY=$CUFFMERGE/assemblies.txt
+
+
+#GTF="/u/home/c/chelseaj/project/database/Ensembl/Homo_sapiens_GRCh37_74_genome.fa"
+#GENOME="/u/home/c/chelseaj/project/database/Ensembl/Genome/Homo_sapiens_GRCh37_74_genome.fa"
+
+GTF="/home/chelseaju/Database/Ensembl/Homo_sapiens.GRCh37.74.gtf"
+GENOME="/home/chelseaju/Database/Ensembl/Genome/Homo_sapiens_GRCh37_74_genome.fa"
+
+## Step 0: Create directory
+echo ""
+echo "Step 0 - Creating Directories"
+echo "mkdir -p $CUFFMERGE"
+mkdir -p $CUFFMERGE
+
+## Step 1: Create Assemblies.txt
+echo ""
+echo "Step 1 - Creating Assemblies.txt"
+echo "$DIR/$SAMPLEA/$CUFFLINK/transcripts.gtf" > $ASSEMBLY
+echo "$DIR/$SAMPLEB/$CUFFLINK/transcripts.gtf" >> $ASSEMBLY
+echo "Write to $ASSEMBLY"
+
+## Step 2: Running cuffmerge
+echo ""
+echo "Step 2 - Running Cuffmerge"
+echo "cuffmerge -o $CUFFMERGE -g $GTF -s $GENOME $ASSEMBLY"
+cuffmerge -o $CUFFMERGE -g $GTF -s $GENOME $ASSEMBLY
+
+## Step 3: Runing cuffdiff
+echo ""
+echo "Step 3 - Running Cuffdiff"
+echo "cuffdiff -o $CUFFMERGE -b $GENOME -L $SAMPLEA,$SAMPLEB -u $CUFFMERGE/merged_asm/merged.gtf ../../$SAMPLEA/$TOPHAT/$BAM ../../$SAMPLEB/%TOPHAT/$BAM"
