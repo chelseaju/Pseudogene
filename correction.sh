3b4738038d271125afddd1b11178fba5d16f828f
chelseaju <chelseaju@ucla.edu>
2014-05-13 22:11:20 -0700

    add correction.sh

--- correction.sh
+++ correction.sh
@@ -21,7 +21,8 @@ LASSO_LOCUS="$DIR/$LASSODIR/$PREFIX""_validationY_locus.txt"
 #ENST2ENSG="/u/home/c/chelseaj/project/database/Ensembl/script/ENST2ENSG.py"
 
 ## LAB
-BED="/home/chelseaju/Database/Pseudogene/ParentENSG_Pseudogene_74.bed"
+PARENT="/home/chelseaju/Database/Ensembl/ENSG_74.bed"
+PSEUDOGENE="/home/chelseaju/Database/Pseudogene/Pseudogene_74.bed"
 #ENST_ENSG_ENSP="/home/chelseaju/Database/Ensembl/ENST_ENSG_ENSP_74.txt"
 #ENST2ENSG="/home/chelseaju/Database/Ensembl/script/ENST2ENSG.py"
 
@@ -38,9 +39,14 @@ echo "Step 2 - Preparing Files"
 perl -pi -e 's/'$PREFIX'_//g' $LASSO_Y
 perl -pi -e 's/'$PREFIX'_//g' $LASSO_X
 
+## format X matrix, remove the "^M sign"
+tr '\015' '\012' < $LASSO_X > $DIR/$PREFIX/$TOPOUT/correction/distribution_matrix.txt
+cp $DIR/$PREFIX/$TOPOUT/correction/distribution_matrix.txt $LASSO_X
+
 ## merge gene ID with location
-join -t $'\t' -1 1 -2 4 <(sort -k1 $LASSO_Y) <(sort -k4 $BED) | cut -f1-5 > $LASSO_LOCUS
-mv $LASSO_LOCUS $DIR/$PREFIX/$TOPOUT/correction/expected_count.txt
+join -t $'\t' -1 1 -2 4 -a 1 -o 0,2.1,2.2,2.3 <(head -1 $LASSO_X | tr '\t' '\n' | sort) <(cat $PSEUDOGENE $PARENT | sort -k 4) > $DIR/$PREFIX/$TOPOUT/correction/locus.txt
+join -t $'\t' -1 1 -2 1 -a 1 <(sort -k 1 $DIR/$PREFIX/$TOPOUT/correction/locus.txt) <(sort -k 1 $LASSO_Y) > $DIR/$PREFIX/$TOPOUT/correction/expected_count.txt
+
 
 echo ""
 echo "Step 3 - Marking Multireads"
@@ -50,3 +56,9 @@ python multiread_marker.py -d $DIR/$PREFIX/$TOPOUT/ -p T
 
 ## count unique reads for each region first
 python uniqueread_assigner.py -d $DIR/$PREFIX/$TOPOUT/ -p T
+
+## assign multireads to diferent regions
+python multiread_assigner.py -d $DIR/$PREFIX/$TOPOUT/ -p T
+
+## re-assign unique reads using dynamic programming
+python uniqueread_reassigner.py -d $DIR/$PREFIX/$TOPOUT/ -p T
